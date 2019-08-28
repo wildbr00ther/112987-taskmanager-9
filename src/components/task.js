d96721268d7119cd47e1962c6c6ad82e0bcf53aa
wildbrother <wildbrother@yandex.ru>
2019-08-28 22:43:44 +0600

    Добавлен объект карточки, фильтра. Созданы функции для их рендеринга. Рендер оставшихся задач по клику на Load more

--- src/components/task.js
+++ src/components/task.js
@@ -1,68 +1,54 @@
-export const getTaskMarkup = () => {
-  return `
-        <article class="card card--black">
-            <div class="card__form">
-              <div class="card__inner">
-                <div class="card__control">
-                  <button type="button" class="card__btn card__btn--edit">
-                    edit
-                  </button>
-                  <button type="button" class="card__btn card__btn--archive">
-                    archive
-                  </button>
-                  <button
-                    type="button"
-                    class="card__btn card__btn--favorites card__btn--disabled"
-                  >
-                    favorites
-                  </button>
-                </div>
-
-                <div class="card__color-bar">
-                  <svg class="card__color-bar-wave" width="100%" height="10">
-                    <use xlink:href="#wave"></use>
-                  </svg>
-                </div>
-
-                <div class="card__textarea-wrap">
-                  <p class="card__text">Example default task with default color.</p>
-                </div>
-
-                <div class="card__settings">
-                  <div class="card__details">
-                    <div class="card__dates">
-                      <div class="card__date-deadline">
-                        <p class="card__input-deadline-wrap">
-                          <span class="card__date">23 September</span>
-                          <span class="card__time">11:15 PM</span>
-                        </p>
-                      </div>
-                    </div>
-
-                    <div class="card__hashtag">
-                      <div class="card__hashtag-list">
-                        <span class="card__hashtag-inner">
-                          <span class="card__hashtag-name">
-                            #todo
-                          </span>
-                        </span>
-
-                        <span class="card__hashtag-inner">
-                          <span class="card__hashtag-name">
-                            #personal
-                          </span>
-                        </span>
-
-                        <span class="card__hashtag-inner">
-                          <span class="card__hashtag-name">
-                            #important
-                          </span>
-                        </span>
-                      </div>
-                    </div>
-                  </div>
-                </div>
+export const getTaskMarkup = ({description, dueDate, repeatingDays, tags, color}) => `
+  <article class="card card--${color} ${Object.keys(repeatingDays).some((day) => repeatingDays[day]) ? `card--repeat` : ``}">
+    <div class="card__form">
+      <div class="card__inner">
+        <div class="card__control">
+          <button type="button" class="card__btn card__btn--edit">
+            edit
+          </button>
+          <button type="button" class="card__btn card__btn--archive">
+            archive
+          </button>
+          <button
+            type="button"
+            class="card__btn card__btn--favorites card__btn--disabled"
+          >
+            favorites
+          </button>
+        </div>
+        <div class="card__color-bar">
+          <svg class="card__color-bar-wave" width="100%" height="10">
+            <use xlink:href="#wave"></use>
+          </svg>
+        </div>
+        <div class="card__textarea-wrap">
+          <p class="card__text">${description}</p>
+        </div>
+        <div class="card__settings">
+          <div class="card__details">
+            <div class="card__dates">
+              <div class="card__date-deadline">
+                <p class="card__input-deadline-wrap">
+                  <span class="card__date">${new Date(dueDate).toDateString()}</span>
+                  <span class="card__time">11:15 PM</span>
+                </p>
               </div>
             </div>
-          </article>`;
-};
+            <div class="card__hashtag">
+              <div class="card__hashtag-list">
+                ${Array.from(tags)
+                  .sort(() => Math.random() - 0.5)
+                  .slice(0, 4)
+                  .map((tag) => `<span class="card__hashtag-inner">
+                      <span class="card__hashtag-name">
+                        #${tag}
+                      </span>
+                    </span>`).join(``)}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </article>
+`.trim();
--- src/main.js
+++ src/main.js
@@ -1,32 +1,68 @@
 import {getMenuMarkup} from './components/menu.js';
 import {getSearchMarkup} from './components/search.js';
-import {getFilterMarkup} from './components/filter.js';
+import {getFiltersMarkup, getFilter} from './components/filter.js';
 import {getBoardMarkup} from './components/board.js';
-import {getFilterListMarkup} from './components/filter-list.js';
-import {getNewTaskMarkup} from './components/new-task.js';
 import {getTaskMarkup} from './components/task.js';
-import {getShowMoreMarkup} from './components/show-more-btn.js';
+import {getTaskEditMarkup} from './components/edit-task.js';
+import {getLoadButtonMarkup} from './components/show-more-btn.js';
+import mockArray from './data.js';
 
-const renderComponent = (containerName, position, template) => {
-  containerName.insertAdjacentHTML(template, position);
+const mainContainer = document.querySelector(`.main`);
+const menuContainer = document.querySelector(`.main__control`);
+export const TASK_COUNT = 9;
+export const tasks = mockArray;
+let tasksForLoad = mockArray;
+
+const renderComponent = (markup, container, repeat = 1, callback = () => null) => {
+  for (let i = 0; i < repeat; i++) {
+    container.insertAdjacentHTML(`beforeend`, markup);
+  }
+  callback();
 };
 
-const siteMainContainer = document.querySelector(`.main`);
-const siteHeaderContainer = siteMainContainer.querySelector(`.main__control`);
+const renderTaskEdit = (container) => {
+  let {description, dueDate, repeatingDays, tags, color} = tasksForLoad[0];
+  container.insertAdjacentHTML(`beforeend`, getTaskEditMarkup({description, dueDate, repeatingDays, tags, color}));
+  tasksForLoad = tasksForLoad.slice(1);
+};
 
-renderComponent(siteHeaderContainer, getMenuMarkup(), `beforeend`);
-renderComponent(siteMainContainer, getSearchMarkup(), `beforeend`);
-renderComponent(siteMainContainer, getFilterMarkup(), `beforeend`);
-renderComponent(siteMainContainer, getBoardMarkup(), `beforeend`);
+const renderTasks = (container, count) => {
+  count = count <= tasksForLoad.length ? count : tasksForLoad.length;
+  for (let i = 0; i < count; i++) {
+    let {description, dueDate, repeatingDays, tags, color} = tasksForLoad[i];
+    container.insertAdjacentHTML(`beforeend`, getTaskMarkup({description, dueDate, repeatingDays, tags, color}));
+  }
+  tasksForLoad = tasksForLoad.slice(count);
+};
 
-const boardContainer = siteMainContainer.querySelector(`.board`);
-const tasksContainer = siteMainContainer.querySelector(`.board__tasks`);
+const renderFilters = (container) => {
+  container.insertAdjacentHTML(`beforeend`, new Array(1)
+    .fill(``)
+    .map(getFilter)
+    .map(getFiltersMarkup)
+    .join(``));
+};
 
-renderComponent(boardContainer, getFilterListMarkup(), `afterbegin`);
 
-renderComponent(tasksContainer, getNewTaskMarkup(), `beforeend`);
-renderComponent(tasksContainer, getTaskMarkup(), `beforeend`);
-renderComponent(tasksContainer, getTaskMarkup(), `beforeend`);
-renderComponent(tasksContainer, getTaskMarkup(), `beforeend`);
+renderComponent(getMenuMarkup(), menuContainer);
+renderComponent(getSearchMarkup(), mainContainer);
+renderFilters(mainContainer);
+renderComponent(getBoardMarkup(), mainContainer, 1, () => {
+  const boardContainer = document.querySelector(`.board`);
+  const taskListContainer = document.querySelector(`.board__tasks`);
 
-renderComponent(boardContainer, getShowMoreMarkup(), `beforeend`);
+  renderTaskEdit(taskListContainer);
+  renderTasks(taskListContainer, TASK_COUNT);
+
+  renderComponent(getLoadButtonMarkup(), boardContainer);
+  const loadMoreButton = document.querySelector(`.load-more`);
+
+  const loadMoreButtonHandler = () => {
+    renderTasks(taskListContainer, TASK_COUNT);
+    if (tasksForLoad.length === 0) {
+      loadMoreButton.removeEventListener(`click`, loadMoreButtonHandler);
+      loadMoreButton.remove();
+    }
+  };
+  loadMoreButton.addEventListener(`click`, loadMoreButtonHandler);
+});
